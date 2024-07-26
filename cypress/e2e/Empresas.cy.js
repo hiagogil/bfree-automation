1b34c0afe75dc5007ed83ea020facd7b6b8c8363
Carla <carla@MacBook-Air-de-Carla.local>
2024-07-26 14:57:09 -0300

    feat: Adicionando funcionalidade Empresas

--- cypress/e2e/Empresas.cy.js
+++ cypress/e2e/Empresas.cy.js
@@ -1,16 +1,14 @@
 import { faker } from "@faker-js/faker";
-
+const empresas = {
+  id_number: faker.number.int({ max: 10000 }),
+  enterprise_name: faker.company.bsAdjective()
+}
 describe('Empresas', () => {
   beforeEach(() => {
     cy.visit('/')
     cy.Login()
-    
   })
-    it('Cadastro de Empresas', () => {
-      const empresas = {
-        id_number: faker.number.int({ max: 10000 }),
-        enterprise_name: faker.company.bsAdjective()
-      }
+    it('Cadastro de Empresas', () => {  
      cy.get('.MuiListItemIcon-root.css-1f8bwsm').eq(0).click()
         cy.get('.btn.btn-register').click()
         cy.get('input[name="cpn_cli_cod"]').type(empresas.id_number)
@@ -18,8 +16,28 @@ describe('Empresas', () => {
         cy.get('[data-cy="companyform-submit-button"]').click()
         cy.contains('Operação realizada com sucesso!').should('be.visible')
     });
-  
- 
+    it('Editar Empresa', () => {
+      cy.intercept('GET', '/api-stage/companies/procurar/**').as('getCompanies')
+      cy.get('.MuiListItemIcon-root.css-1f8bwsm').eq(0).click()
+      cy.get(':nth-child(1) > .sc-bmzXxz > a > .btn').click()
+      cy.wait('@getCompanies')
+      cy.get('input[name="cpn_cli_cod"]').clear().type(empresas.id_number)
+      cy.get('input[name="cpn_name"]').clear().type(empresas.enterprise_name)
+      cy.get('[data-cy="companyform-submit-button"]').click()
+      cy.contains('Operação realizada com sucesso!').should('be.visible')
+    });
+    it('Erro ao excluir uma empresa', () => {
+      cy.get('.MuiListItemIcon-root.css-1f8bwsm').eq(0).click()
+      cy.get('.sc-hmdnzv > :nth-child(1) > .sc-bmzXxz > :nth-child(2)').click()
+      cy.get('.modal-footer > .btn-dark').click()
+      cy.contains('Não é possivel excluir o registro, pois o mesmo possui ligação com outras tabelas!').should('be.visible')
+    });
+    it('Sucesso ao excluir uma empresa', () => {
+      cy.get('.MuiListItemIcon-root.css-1f8bwsm').eq(0).click()
+      cy.get(':nth-child(4) > .sc-bmzXxz > :nth-child(2)').click()
+      cy.get('.modal-footer > .btn-dark').click()
+      cy.contains('Operação realizada com sucesso!').should('be.visible')
+    });
 });
  
  
